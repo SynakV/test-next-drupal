af5ed98023d0f4c700f7c409c66b92926bd2fc4b
Vasyl.Sunak <vasyl.sunak@vsunak-MacBook-Pro.local>
2022-08-11 16:18:09 +0300

    Configured pwa

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
-
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
   images: {
     domains: [process.env.NEXT_IMAGE_DOMAIN],
   },
-  pwa: {
-    dest: 'public',
-    runtimeCaching
-  }
 });
